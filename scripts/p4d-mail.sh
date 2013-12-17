90cffcb3b65d29dfb8584cf9a2869827a085fe0c
horchi <vdr@jwendel.de>
2013-12-17 16:14:48 +0100

    improved mailscript

--- scripts/p4d-mail.sh
+++ scripts/p4d-mail.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 
-echo "$2" | iconv -f utf-8 -t iso-8859-1 | mail -s "$1" $3
+echo "$2" | mail -s "$1" -a "Content-Type: text/plain; charset=UTF-8" $3
+
+#echo "$2" | iconv -f utf-8 -t iso-8859-1 | mail -s "$1" $3
+
+
 
