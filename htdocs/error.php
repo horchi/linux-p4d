919a0bc0808704c80bda5227966b054c3dccd708
horchi <vdr@jwendel.de>
2016-12-19 13:37:39 +0100

    minor css/web change

--- htdocs/error.php
+++ htdocs/error.php
@@ -12,38 +12,21 @@ $mysqli->query("set names 'utf8'");
 $mysqli->query("SET lc_time_names = 'de_DE'");
 
 
-showTable("Fehlerspeicher");
-
 //***************************************************************************
 // Show Table
 //***************************************************************************
 
-function showTable($tableTitle)
-{
-   global $mysqli;
-
    $i = 0;
 
-   echo "      <br/>\n";
-   echo "      <table class=\"tableTitle\" cellspacing=0 rules=rows>\n";
-   echo "        <tr>\n";
-   echo "          <td><center><b>$tableTitle</b></center></td>\n";
-   echo "        </tr>\n";
-   echo "      </table>\n";
-   echo "      <br/>\n";
-
-   echo "      <table  class=\"tableLight\" border=1 cellspacing=0 rules=rows>
-        <tr class=\"tableHead\">
-          <td> Zeit </td>
-          <td> Nr </td>
-          <td> Status </td>
-          <td> Fehler </td>
-        </tr>\n";
+   seperator("Fehlerspeicher", 0);
 
    $result = $mysqli->query("select * from errors order by time desc")
       or die("<br/>Error" . $mysqli->error);
 
    $num = $result->num_rows;
+
+   echo "      <div class=\"rounded-border tableMainMM\">\n";
+   echo "        <center>Messwerte vom $maxPretty</center>\n";
 
    while ($i < $num)
    {
@@ -52,23 +35,18 @@ function showTable($tableTitle)
       $state   = mysqli_result($result, $i, "state");
       $text    = mysqli_result($result, $i, "text");
 
-      if ($i % 2)
-         echo "         <tr class=\"tableLight\">";
-      else
-         echo "         <tr class=\"tableDark\">";
-
-      echo "
-           <td> $time </td>
-           <td> $nr </td>
-           <td> $state </td>
-           <td> $text </td>\n";
-      echo "        </tr>\n";
+      echo "         <div>\n";
+      echo "           <span>$time</span>\n";
+      echo "           <span>$nr</span>\n";
+      echo "           <span>$state</span>\n";
+      echo "           <span>$text</span>\n";
+      echo "         </div>\n";
 
       $i++;
    }
 
-   echo "      </table>\n";
-}
+   echo "      </div>\n";  // tableMainMM
+
 
 include("footer.php");
 
