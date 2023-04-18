4ba6564a6c4b37ee9a68d3f331159bfacd4e8f1e
horchi <vdr@jwendel.de>
2023-04-18 18:49:05 +0200

    minor fix

--- wsactions.c
+++ wsactions.c
@@ -683,7 +683,7 @@ int Daemon::performSystem(json_t* oObject, long client)
       json_object_set_new(jItem, "name", json_string(tableTableStatistics->getStrValue("NAME")));
       json_object_set_new(jItem, "tblsize", json_string(bytesPretty(tableTableStatistics->getBigintValue("DATASZ"), 2)));
       json_object_set_new(jItem, "idxsize", json_string(bytesPretty(tableTableStatistics->getBigintValue("INDEXSZ"), 2)));
-      json_object_set_new(jItem, "rows", json_integer(tableTableStatistics->getIntValue("ROWS")));
+      json_object_set_new(jItem, "rows", json_integer(tableTableStatistics->getBigintValue("ROWS")));
    }
 
    selectTableStatistic->freeResult();
